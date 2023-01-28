cf7c032470fecf574b88772b540a6cc4f8b973a3
Jhon Costa <jhon.costa.dev@gmail.com>
2023-01-28 14:55:00 -0300

    Início da lógica
    Referenciando as tags:
    input
    btn
    lista

---
+++ Projeto-Lista-de-Tarefa/javascript/logica.js
@@ -0,0 +1,6 @@
+// Referenciar o input
+let input = document.querySelector('input[name=tarefa]')
+// Referenciar o button
+let btn = document.querySelector('#botao')
+// Referenciar a lista
+let lista = document.querySelector('#lista')
